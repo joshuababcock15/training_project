92b2ba3e6a096faa0765d549dbf5fdb6a2f89c34
joshuababcock15 <Joshuababcock15@gmail.com>
2024-11-13 16:25:50 -0500

    update

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import cors from "cors";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { testMiddleware }   from "./middleware/test";
 import { usersRouter } from "./routers";
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Go Bills!')
-});
-
-app.use(express.json());
 
 app.use(
   cors({
@@ -16,6 +13,28 @@ app.use(
   })
 );
 
+app.use(express.json());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Training Project API",
+      version: "1.0.0",
+      description:
+        "This is the Swagger API documentation for the Training Project API.",
+    },
+  },
+  apis: ["./routers/*.ts"], // files containing annotations as above
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+console.log("swaggerSpec", swaggerSpec);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("*", testMiddleware);
+app.get('/', (req, res) => {
+  res.send('Go Bills!')
+});
 app.use("/users",  usersRouter);
 
 export default app;
--- server/routers/users.ts
+++ server/routers/users.ts
@@ -1,33 +1,77 @@
 import { Router } from "express";
 import { deleteUsers, getUser, getUsers, postUsers, putUsers } from "../controllers/users";
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: The Users managing API
+ */
 const router: any = Router();
 
+// GET all users
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     description: Get a list of all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a JSON list of all the users
+ */
 router.route("/").get(getUsers);
 
+// POST a user
+/**
+ * @openapi
+ * /users:
+ *   post:
+ *     description: Post a user
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a string
+ */
 router.route("/").post(postUsers);
 
-// router.route("/").post((req: any, res: any) => {
-//   res.send("Posted to users!");
-// });
-
+// PUT a user
+/**
+ * @openapi
+ * /users/{id}:
+ *   put:
+ *     description: Put a user
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a string
+ */
 router.route("/:id").put(putUsers);
 
-// router.route("/:id").put((req: any, res: any) => {
-//   res.send("Put to users!");
-// });
-
+// DELETE a user
+/**
+ * @openapi
+ * /users/{id}:
+ *   delete:
+ *     description: Delete a user
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a string
+ */
 router.route("/:id").delete(deleteUsers);
 
-// router.route("/:id").delete((req: any, res:any) => {
-//   res.send("Deleted users!");
-// });
-
+// GET a user
+/**
+ * @openapi
+ * /users/{id}:
+ *   get:
+ *     description: Get a user
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Returns a string
+ */
 router.route("/:id").get(getUser);
-
-// router.route("/:id").get((req:any, res:any) => {
-//   res.send("Got a user!");
-// });
-
 
 export default router;
